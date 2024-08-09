a7277d4f9e97deb9198c09587c6f7a246e374b30
진상우 <tkddnrj321@naver.com>
2024-08-09 18:18:44 +0900

    prisma supabase 연동 및 migration

--- app/posts.tsx
+++ app/posts.tsx
@@ -1,3 +1,60 @@
+"use client";
+const posts = [
+    {
+        id: "first post",
+        date: "2024-08-05",
+        title: "My first post",
+    },
+    {
+        id: "two",
+        date: "2024-08-05",
+        title: "text2",
+    },
+    {
+        id: "three",
+        date: "2024-08-05",
+        title: "test3",
+    },
+    {
+        id: "four",
+        date: "2024-08-05",
+        title: "test4",
+    },
+    {
+        id: "five",
+        date: "2024-08-05",
+        title: "test5",
+    },
+    {
+        id: "six",
+        date: "2024-08-05",
+        title: "test6",
+    },
+    {
+        id: "seven",
+        date: "2024-08-05",
+        title: "test7",
+    },
+    {
+        id: "eight",
+        date: "2024-08-05",
+        title: "test8",
+    },
+    {
+        id: "nine",
+        date: "2024-08-05",
+        title: "test9",
+    },
+    {
+        id: "ten",
+        date: "2024-08-05",
+        title: "test10",
+    },
+];
+
+type SortSetting = ["date" | "views", "desc" | "asc"];
+console.log(posts);
+
 export function Posts() {
     return (
         <main className="max-w-2xl m-auto font-mono text-sm ">
@@ -12,5 +69,14 @@ export function Posts() {
 }
 
 function List() {
-    return <div>hellog</div>;
+    return (
+        <section className="flex flex-col font-mono">
+            {posts.map((post) => (
+                <ul key={post.id} className="flex">
+                    <li className="flex">{post.date}</li>
+                    <li className="flex grow">{post.title}</li>
+                </ul>
+            ))}
+        </section>
+    );
 }
